a84714e1ca5b09156c18116f5d4470631e5906b5
Yann Vander Meersche <yann-vm@hotmail.fr>
2020-10-15 17:36:31 +0200

    11/16

--- debruijn/debruijn.py
+++ debruijn/debruijn.py
@@ -17,20 +17,20 @@ import argparse
 import os
 import sys
 import networkx as nx
-import matplotlib
+import matplotlib.pyplot as plt
 from operator import itemgetter
 import random
 random.seed(9001)
 from random import randint
 import statistics
 
-__author__ = "Your Name"
+__author__ = "Vander Meersche Yann"
 __copyright__ = "Universite Paris Diderot"
-__credits__ = ["Your Name"]
+__credits__ = ["Yann Vander Meersche"]
 __license__ = "GPL"
 __version__ = "1.0.0"
-__maintainer__ = "Your Name"
-__email__ = "your@email.fr"
+__maintainer__ = "VAnder Meersche Yann"
+__email__ = "yann-vm@hotmail.fr"
 __status__ = "Developpement"
 
 def isfile(path):
@@ -65,6 +65,85 @@ def get_arguments():
     return parser.parse_args()
 
 
+def read_fastq(fastq_file):
+    with open(fastq_file, "r") as f_in:
+        for line in f_in:
+            yield next(f_in).strip()
+            next(f_in)
+            next(f_in)
+
+
+def cut_kmer(fasta_seq, k):
+    for i in range(len(fasta_seq) - k+1):
+        yield fasta_seq[i:i+k]
+
+
+def build_kmer_dict(fastq_file, k):
+    kmer_dict = {}
+    for fasta_seq in read_fastq(fastq_file):
+        for kmer in cut_kmer(fasta_seq, k):
+            if kmer not in kmer_dict :
+                kmer_dict[kmer] = 0
+            kmer_dict[kmer] += 1
+    return kmer_dict
+
+
+def build_graph(kmer_dict):
+    G = nx.DiGraph()
+    for kmer in kmer_dict:
+        G.add_edge(kmer[:-1], kmer[1:], weight=kmer_dict[kmer])
+    #nx.draw(G, with_labels=False)
+    #plt.show()
+    return G
+
+
+def get_starting_nodes(G):
+    start_nodes = []
+    for node in G.nodes:
+        if len(list(G.predecessors(node))) == 0:
+            start_nodes.append(node)
+    return start_nodes
+
+
+def get_sink_nodes(G):
+    sink_nodes = []
+    for node in G.nodes:
+        if len(list(G.successors(node))) == 0:
+            sink_nodes.append(node)
+    return sink_nodes
+
+
+def get_contigs(G, start_nodes, sink_nodes):
+    contigs_list = []
+
+    for start in start_nodes:
+        for sink in sink_nodes:
+            for path in nx.all_simple_paths(G, source=start, target=sink):
+                contig = ""
+
+                for kmer in path:
+                    if not contig:
+                        contig += kmer
+                    else:
+                        contig += kmer[-1]
+                contigs_list.append((contig, len(contig)))
+
+    return contigs_list
+
+
+def fill(text, width=80):
+    """Split text with a line return to respect fasta format"""
+    return os.linesep.join(text[i:i+width] for i in range(0, len(text), width))
+
+
+def save_contigs(contig_list, contig_filename):
+    with open(contig_filename, "w") as f_out:
+        for i, contig in enumerate(contig_list):
+            f_out.write(f">contig_{i} len={contig[1]}\n")
+            f_out.write(fill(contig[0]) + "\n")
+
+
+
 #==============================================================
 # Main program
 #==============================================================
@@ -74,6 +153,13 @@ def main():
     """
     # Get arguments
     args = get_arguments()
+    kmer_dict = build_kmer_dict(args.fastq_file, args.kmer_size)
+    G = build_graph(kmer_dict)
+    start_nodes = get_starting_nodes(G)
+    sink_nodes = get_sink_nodes(G)
+    contig_list = get_contigs(G, start_nodes, sink_nodes)
+    
+    save_contigs(contig_list, args.output_file)
 
 if __name__ == '__main__':
     main()
