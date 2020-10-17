bf0e5b961869af7d2339d03063d2aa75f9dd2aa6
Yann Vander Meersche <yann-vm@hotmail.fr>
2020-10-17 13:30:20 +0200

    PEP8

--- debruijn/debruijn.py
+++ debruijn/debruijn.py
@@ -1,3 +1,6 @@
+"""Perform assembly based on debruijn graph."""
+
+
 #!/bin/env python3
 # -*- coding: utf-8 -*-
 #    This program is free software: you can redistribute it and/or modify
@@ -11,18 +14,16 @@
 #    A copy of the GNU General Public License is available at
 #    http://www.gnu.org/licenses/gpl-3.0.html
 
-"""Perform assembly based on debruijn graph."""
-
-import argparse
 import os
 import sys
-import networkx as nx
-import matplotlib.pyplot as plt
-from operator import itemgetter
 import random
-random.seed(9001)
-from random import randint
+import argparse
 import statistics
+
+import networkx as nx
+
+# Set the random seed
+random.seed(9001)
 
 __author__ = "Vander Meersche Yann"
 __copyright__ = "Universite Paris Diderot"
@@ -32,6 +33,7 @@ __version__ = "1.0.0"
 __maintainer__ = "Vander Meersche Yann"
 __email__ = "yann-vm@hotmail.fr"
 __status__ = "Developpement"
+
 
 def isfile(path):
     """Check if path is an existing file.
@@ -52,8 +54,7 @@ def get_arguments():
       Returns: An object that contains the arguments
     """
     # Parsing arguments
-    parser = argparse.ArgumentParser(description=__doc__, usage=
-                                     "{0} -h"
+    parser = argparse.ArgumentParser(description=__doc__, usage="{0} -h"
                                      .format(sys.argv[0]))
     parser.add_argument('-i', dest='fastq_file', type=isfile,
                         required=True, help="Fastq file")
@@ -77,7 +78,7 @@ def read_fastq(fastq_file):
     Generator yielding Fasta reads
     """
     with open(fastq_file, "r") as f_in:
-        for line in f_in:
+        for _ in f_in:
             yield next(f_in).strip()
             next(f_in)
             next(f_in)
@@ -113,7 +114,7 @@ def build_kmer_dict(fastq_file, k):
     kmer_dict = {}
     for fasta_seq in read_fastq(fastq_file):
         for kmer in cut_kmer(fasta_seq, k):
-            if kmer not in kmer_dict :
+            if kmer not in kmer_dict:
                 kmer_dict[kmer] = 0
             kmer_dict[kmer] += 1
     return kmer_dict
@@ -128,56 +129,56 @@ def build_graph(kmer_dict):
 
     Returns
     -------
-    G: NetworkX graph
+    graph: NetworkX graph
     """
-    G = nx.DiGraph()
+    graph = nx.DiGraph()
     for kmer in kmer_dict:
-        G.add_edge(kmer[:-1], kmer[1:], weight=kmer_dict[kmer])
-    return G
+        graph.add_edge(kmer[:-1], kmer[1:], weight=kmer_dict[kmer])
+    return graph
 
 
-def get_starting_nodes(G):
+def get_starting_nodes(graph):
     """Get the starting nodes from the graph.
 
     Arguments
     ---------
-    G: NetworkX graph
+    graph: NetworkX graph
 
     Returns
     -------
     start_nodes: Starting nodes list
     """
     start_nodes = []
-    for node in G.nodes:
-        if len(list(G.predecessors(node))) == 0:
+    for node in graph.nodes:
+        if len(list(graph.predecessors(node))) == 0:
             start_nodes.append(node)
     return start_nodes
 
 
-def get_sink_nodes(G):
+def get_sink_nodes(graph):
     """Get the sinking nodes from the graph.
 
     Arguments
     ---------
-    G: NetworkX graph
+    graph: NetworkX graph
 
     Returns
     -------
     sink_nodes: Sinking nodes list
     """
     sink_nodes = []
-    for node in G.nodes:
-        if len(list(G.successors(node))) == 0:
+    for node in graph.nodes:
+        if len(list(graph.successors(node))) == 0:
             sink_nodes.append(node)
     return sink_nodes
 
 
-def get_contigs(G, start_nodes, sink_nodes):
+def get_contigs(graph, start_nodes, sink_nodes):
     """Get all the contigs between the staring nodes and the sinking nodes.
 
     Arguments
     ---------
-    G: NetworkX graph
+    graph: NetworkX graph
     start_nodes: Start nodes list
     sink_nodes: Sink nodes list
 
@@ -189,9 +190,8 @@ def get_contigs(G, start_nodes, sink_nodes):
 
     for start in start_nodes:
         for sink in sink_nodes:
-            for path in nx.all_simple_paths(G, source=start, target=sink):
+            for path in nx.all_simple_paths(graph, source=start, target=sink):
                 contig = ""
-
                 for kmer in path:
                     if not contig:
                         contig += kmer
@@ -225,26 +225,26 @@ def save_contigs(contig_list, contig_filename):
             f_out.write(fill(contig[0]) + "\n")
 
 
-def std(list):
+def std(value_list):
     """Calculate the standard deviation of a list.
 
     Arguments
     ---------
-    list: List of values
+    value_list: List of values
 
     Returns
     -------
     Standard deviation of the list
     """
-    return statistics.stdev(list)
+    return statistics.stdev(value_list)
 
 
-def path_average_weight(G, path):
+def path_average_weight(graph, path):
     """Calculate the average weight of a path.
 
     Arguments
     ---------
-    G: NetworkX graph
+    graph: NetworkX graph
     path: Path
 
     Returns
@@ -253,52 +253,53 @@ def path_average_weight(G, path):
     """
     weight = 0
     for i in range(len(path) - 1):
-        weight += G.edges[path[i], path[i+1]]["weight"]
+        weight += graph.edges[path[i], path[i+1]]["weight"]
 
     return weight / (len(path) - 1)
 
 
-def remove_paths(G, path_list, delete_entry_node, delete_sink_node):
+def remove_paths(graph, path_list, delete_entry_node, delete_sink_node):
     """Remove all the path of a graph from a list with or witout removing
     the entry or sinking nodes.
 
     Arguments
     ---------
-    G: NetworkX graph
+    graph: NetworkX graph
     path_list: Path list
-    delete_entry_node: Entry node deletion boolean 
-    delete_sink_node: Sink node deletion boolean 
+    delete_entry_node: Entry node deletion boolean
+    delete_sink_node: Sink node deletion boolean
 
     Returns
     -------
-    G: NetworkX graph
+    graph: NetworkX graph
     """
     for path in path_list:
-        G.remove_nodes_from(path[1:-1])
+        graph.remove_nodes_from(path[1:-1])
         if delete_entry_node:
-            G.remove_node(path[0])
+            graph.remove_node(path[0])
         if delete_sink_node:
-            G.remove_node(path[-1])
-    return G
+            graph.remove_node(path[-1])
+
+    return graph
 
 
-def select_best_path(G, path_list, path_length, path_weight,
-                     delete_entry_node = False, delete_sink_node = False):
+def select_best_path(graph, path_list, path_length, path_weight,
+                     delete_entry_node=False, delete_sink_node=False):
     """Select the best path all the path from a list with or witout removing
     the entry or sinking nodes.
 
     Arguments
     ---------
-    G: NetworkX graph
+    graph: NetworkX graph
     path_list: Path list
     path_length: Path length
     path_weight: Path weight
-    delete_entry_node: Entry node deletion boolean 
-    delete_sink_node: Sink node deletion boolean 
+    delete_entry_node: Entry node deletion boolean
+    delete_sink_node: Sink node deletion boolean
 
     Returns
     -------
-    G: NetworkX graph
+    graph: NetworkX graph
     """
     # Weights
     path_list_wmax = []
@@ -324,7 +325,7 @@ def select_best_path(G, path_list, path_length, path_weight,
 
     # Random
     while len(path_list_lmax) > 1:
-        path_list_lmax.pop(statistics.randint(path_list_lmax))
+        path_list_lmax.pop(random.randint(0, len(path_list_lmax)))
 
     wrong_paths = []
 
@@ -332,82 +333,81 @@ def select_best_path(G, path_list, path_length, path_weight,
         if path not in path_list_lmax:
             wrong_paths.append(path)
 
-    G = remove_paths(G, wrong_paths, delete_entry_node, delete_sink_node)
+    graph = remove_paths(graph, wrong_paths, delete_entry_node, delete_sink_node)
 
-    return G
+    return graph
 
 
-
-def solve_bubble(G, a_node, d_node):
+def solve_bubble(graph, a_node, d_node):
     """Select the best path between two nodes of a bubble and remove the others.
 
     Arguments
     ---------
-    G: NetworkX graph
+    graph: NetworkX graph
     a_node: Ancestor node
     b_node: Descendant node
 
     Returns
     -------
-    G: NetworkX graph
+    graph: NetworkX graph
     """
     path_list = []
     path_length = []
     path_weight = []
 
-    for path in nx.all_simple_paths(G, source=a_node, target=d_node):
+    for path in nx.all_simple_paths(graph, source=a_node, target=d_node):
         path_list.append(path)
         path_length.append(len(path))
-        path_weight.append(path_average_weight(G, path))
+        path_weight.append(path_average_weight(graph, path))
 
-    G = select_best_path(G, path_list, path_length, path_weight,
-                         delete_entry_node = False, delete_sink_node = False)
+    graph = select_best_path(graph, path_list, path_length, path_weight,
+                         delete_entry_node=False, delete_sink_node=False)
 
-    return G
+    return graph
 
 
-def simplify_bubbles(G):
+def simplify_bubbles(graph):
     """Search for bubbles in the graph and simplify them.
 
     Arguments
     ---------
-    G: NetworkX graph
+    graph: NetworkX graph
 
     Returns
     -------
-    G: NetworkX graph
+    graph: NetworkX graph
     """
     wrong_nodes = []
 
-    for d_node in G.nodes:
-        pred_list = list(G.predecessors(d_node))
+    for d_node in graph.nodes:
+        pred_list = list(graph.predecessors(d_node))
         if len(pred_list) > 1:
-            a_node = nx.lowest_common_ancestor(G, pred_list[0], pred_list[1])
+            a_node = nx.lowest_common_ancestor(graph, pred_list[0], pred_list[1])
             wrong_nodes.append([a_node, d_node])
 
     for nodes_ad in wrong_nodes:
-        G = solve_bubble(G, nodes_ad[0], nodes_ad[1])
+        graph = solve_bubble(graph, nodes_ad[0], nodes_ad[1])
 
-    return G
+    return graph
 
 
-def solve_entry_tips(G, start_nodes):
+def solve_entry_tips(graph, start_nodes):
     """Remove the non optimal entry tips.
 
     Arguments
     ---------
-    G: NetworkX graph
+    graph: NetworkX graph
     start_nodes: List of starting nodes
 
     Returns
     -------
-    G: NetworkX graph
+    graph: NetworkX graph
     """
     mult_ancestors = []
-    
+
     for node in start_nodes:
-        for next_node in nx.descendants(G, node):
-            if len(G.pred[next_node]) >= 2 and next_node not in mult_ancestors:
+        for next_node in nx.descendants(graph, node):
+            if len(graph.pred[next_node]) >= 2 and next_node not in mult_ancestors:
                 mult_ancestors.append(next_node)
 
     paths_list = []
@@ -416,34 +416,34 @@ def solve_entry_tips(G, start_nodes):
 
     for node in start_nodes:
         for node2 in mult_ancestors:
-            for path in nx.all_simple_paths(G, node, node2):
-                    paths_list.append(path)
-                    path_length.append(len(path))
-                    path_weight.append(path_average_weight(G, path))
+            for path in nx.all_simple_paths(graph, node, node2):
+                paths_list.append(path)
+                path_length.append(len(path))
+                path_weight.append(path_average_weight(graph, path))
 
-        G = select_best_path(G, paths_list, path_length, path_weight,
+        graph = select_best_path(graph, paths_list, path_length, path_weight,
                              delete_entry_node=True, delete_sink_node=False)
 
-    return G
+    return graph
 
 
-def solve_out_tips(G, sink_nodes):
+def solve_out_tips(graph, sink_nodes):
     """Remove the non optimal sinking tips.
 
     Arguments
     ---------
-    G: NetworkX graph
+    graph: NetworkX graph
     sink_nodes: List of sinking nodes
 
     Returns
     -------
-    G: NetworkX graph
+    graph: NetworkX graph
     """
     mult_descendants = []
-    
+
     for node in sink_nodes:
-        for next_node in nx.ancestors(G, node):
-            if len(G.succ[next_node]) >= 2 and next_node not in mult_descendants:
+        for next_node in nx.ancestors(graph, node):
+            if len(graph.succ[next_node]) >= 2 and next_node not in mult_descendants:
                 mult_descendants.append(next_node)
 
     paths_list = []
@@ -452,22 +452,20 @@ def solve_out_tips(G, sink_nodes):
 
     for node in sink_nodes:
         for node2 in mult_descendants:
-            for path in nx.all_simple_paths(G, node2, node):
-                    paths_list.append(path)
-                    path_length.append(len(path))
-                    path_weight.append(path_average_weight(G, path))
+            for path in nx.all_simple_paths(graph, node2, node):
+                paths_list.append(path)
+                path_length.append(len(path))
+                path_weight.append(path_average_weight(graph, path))
 
-        G = select_best_path(G, paths_list, path_length, path_weight,
+        graph = select_best_path(graph, paths_list, path_length, path_weight,
                              delete_entry_node=False, delete_sink_node=True)
 
-    return G
+    return graph
 
 
-
-
-#==============================================================
+# ==============================================================
 # Main program
-#==============================================================
+# ==============================================================
 def main():
     """
     Main program function
@@ -479,21 +477,21 @@ def main():
     kmer_dict = build_kmer_dict(args.fastq_file, args.kmer_size)
 
     # Build the associated graph
-    G = build_graph(kmer_dict)
+    graph = build_graph(kmer_dict)
 
     # Get the starting and sinking node of this graph.
-    start_nodes = get_starting_nodes(G)
-    sink_nodes = get_sink_nodes(G)
+    start_nodes = get_starting_nodes(graph)
+    sink_nodes = get_sink_nodes(graph)
 
     # Remove all of it's bubbles
-    G = simplify_bubbles(G)
+    graph = simplify_bubbles(graph)
 
     # Remove the non optimal entry and out tips
-    G = solve_entry_tips(G, start_nodes)
-    G = solve_out_tips(G, sink_nodes)
+    graph = solve_entry_tips(graph, start_nodes)
+    graph = solve_out_tips(graph, sink_nodes)
 
     # Get the contigs and save it in a Fasta format
-    contig_list = get_contigs(G, start_nodes, sink_nodes)
+    contig_list = get_contigs(graph, start_nodes, sink_nodes)
     save_contigs(contig_list, args.output_file)
 
 
